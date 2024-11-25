e6aeccea70e19c5204f3d96e95667f9fdd87881f
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-11-25 04:09:09 -0400

    set up endpoints for fetching a user profile with guard clauses, and for adding a user to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config()
 const { Socket } = require('socket.io');
 
 const io = require('socket.io')(2000, {
@@ -41,3 +42,74 @@ const io = require('socket.io')(2000, {
         console.log(`Client disconnected: ${socket.id}`);
       });
   });
+
+
+  const express = require("express");
+  const app = express();
+
+  const cors = require("cors");
+  const pool = require("./DB");
+  
+  
+  
+  app.use(cors());
+  app.use(express.json());
+  console.log(process.env);
+  console.log(process.env.DB_PASSWORD);
+  console.log( String(process.env.DB_PASSWORD))
+
+app.get('/users/all',async(req,res)=>{
+  try {
+    const allUsers = await pool.query("SELECT * FROM users");
+    console.log(allUsers.rows)
+    return res.json(allUsers.rows)
+  } catch (error) {
+    console.log(error.message)
+  }
+})
+  //login endpoint
+  app.get('/users/login', async(req, res)=>{
+    
+   const {username, user_password} = req.query;
+    try {
+      const users = await pool.query('SELECT * FROM users where username = $1 AND user_password = $2',[username,user_password]);
+      if (users.rowCount > 0) {
+        return res.status(200).json({ message: 'Login successful', user: users.rows[0] });
+      } else {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  })
+
+
+//sign up
+  app.post('/users', async(req, res)=>{
+   const {username, user_password} = req.body;
+    try {
+      const existingUsers = (await pool.query("SELECT * FROM users where username = $1", [username]));
+      if(existingUsers.rows.length === 0){
+
+        const users = await pool.query('INSERT INTO users (id, username, user_password) values (default, $1,$2)', [username, user_password]);
+        console.log(users.rows)
+        return res.json(users.rows)
+      } 
+      return res.status(400).json({ message: "Username already exists" });
+
+
+    } catch (error) {
+      console.error('Error adding user:', error);
+      if (error.code === '23505') {
+        res.status(409).json({ error: 'user already exists' });
+      } else {
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    }
+  })
+//   app.listen(process.env.DB_PORT,()=>{
+//     console.log(`server has started on port ${process.env.DB_PORT} `);
+// })
+app.listen(1000,()=>{
+  console.log(`server has started on port ${1000} `);
+})
