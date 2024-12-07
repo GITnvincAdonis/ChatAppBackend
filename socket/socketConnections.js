7cb38cb3f810441381bbfad586ac61ffe0c7a11a
GITnvincAdonis <rav30nad0nis@gmail.com>
2024-12-07 16:15:02 -0400

    migrating adding message to db using socket connection. This is to facilitate the message status

--- socket/socketConnections.js
+++ socket/socketConnections.js
@@ -1,3 +1,4 @@
+const pool = require("../db/DB");
 function SetupSocket(){
     const io = require('socket.io')(2000, {
         cors: {
@@ -5,40 +6,31 @@ function SetupSocket(){
         },
       });
       
-      
       io.on('connection', (socket) => {
         console.log(`Client connected: ${socket.id}`);
-      
-        socket.on("action", (message) => {
-          console.log("Received message:", message);
-          //socket.broadcast.emit("send-to-client",  message);
-    
-       
-        });
-       
-    
-        socket.on("send-to-server", ({ roomName, text }) => {
-            console.log(`Message to room ${roomName}: ${text}`);
-            //socket.to('yo').emit("send-to-client", { text});
-          
-            socket.to(roomName).emit("send-to-client", { text});
-        });
-    
-        socket.on('join-group', (object)=>{
-            console.log(`object: ${object}, socket id: ${socket.id}`)
-            console.log(`joined-group: ${object.group_name}`)
-            socket.join(`${object.group_name}`)
-            //socket.join(`yo`)
+     
+        socket.on("send-to-server", async({ roomName, text,grp_ID,user_ID },cb) => {
+            try {
+              const newMessage = await pool.query(
+               `INSERT INTO messages (message_string, group_id, user_id)
+               values ($1,$2,$3) RETURNING message_id
+               `,[text,grp_ID,user_ID]);
+
+              console.log("New message id:", newMessage.rows[0]);
+
+              await socket.to(roomName).emit("send-to-client", { text});
+              
+              cb("server callback")
+           
+            } catch (error) {
+              console.log(error)
+            }
             
-        }) 
-        socket.on('leave-group', (object)=>{
-            console.log(`object: ${object}, socket id: ${socket.id}`)
-            console.log(`left group: ${object.group_name}`)
-            socket.leave(`${object.group_name}`)
-        }) 
-        socket.on("disconnect", () => {
-            console.log(`Client disconnected: ${socket.id}`);
-          });
+        });
+    
+        socket.on('join-group', (object)=>{socket.join(`${object.group_name}`)}) 
+        socket.on('leave-group', (object)=>{socket.leave(`${object.group_name}`)}) 
+        socket.on("disconnect", () => { console.log(`Client disconnected: ${socket.id}`)});
       });
     
 }
